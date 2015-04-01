5c6e98e7b9054a23c2ff31c6e25d7eb901b9858c
mudongliang <mudongliangabcd@gmail.com>
2015-04-01 11:28:47 +0800

    add classification of usage of dumplicate init function example

--- clock.cpp
+++ clock.cpp
@@ -14,6 +14,9 @@ class Clock{
 		Clock(Clock &clock);
 		void setTime(int newH = 0, int newM = 0, int newS = 0);
 		void showTime();
+		int getHour();
+		int getMinute();
+		int getSecond();
 	private:
 		int hour, minute, second;
 };
@@ -42,6 +45,27 @@ inline void Clock::showTime(){
 	cout<<hour<<":"<<minute<<":"<<second<<endl;
 }
 
+inline int Clock::getHour(){
+	return hour;
+} 
+
+inline int Clock::getMinute(){
+	return minute;
+} 
+
+inline int Clock::getSecond(){
+	return second;
+} 
+
+bool isAm(Clock c){ //require 24 hour 
+	return (c.getHour() < 12);
+}
+/*
+Clock create(){
+	Clock c(1,2,3);
+	return c;
+}
+*/
 int main(){ 
 	Clock myclock(10,22,00);
 	myclock.showTime();
@@ -51,5 +75,20 @@ int main(){
 	cout<<"Second time set and output:";
 	myclock.setTime(10,22,30);
 	myclock.showTime();
+
+	//show how the dumplicate init function runs
+	//1st classification
+	Clock dump(myclock);
+	dump.showTime();
+	Clock twodump = dump;
+	twodump.showTime();
+	
+	//2nd classification
+	if (isAm(dump)){
+		cout<<"This time is am!"<<endl;
+	}
+	//3rd classification
+	//Clock ct = create();
+	//ct.showTime();
     return 0;
 }
